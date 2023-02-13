bbee246d37a84a3e59cd3378541ab99a7bd1eaa9
Muhammad Faizan <faizanyousaf815@gmail.com>
2023-02-14 01:07:08 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,94 @@
+// **1**
+/*let name , admin;
+name = "Umair Azmat";
+admin = name
+console.log(name , admin);
+alert("Hey, It's "+ admin);
+
+**2**
+let planetName;
+
+**3**
+let visitorName;
+
+**4**
+let favDrink = "Ganny ka juice";
+
+**5**
+let favNum = 13; */ 
+
+// **6**
+/* function myFavs() {
+    var favLanguage = "Python";
+    var favTecnology = "AI"
+
+    console.log("My favorite programming language is " + favLanguage);
+    console.log("My favorite Technology is " + favTecnology);
+
+}
+
+myFavs(); */
+
+// **7**
+let person = {
+    name: "Umair",
+    age: 20
+  };
+
+  
+// **8**
+// let num1 , num2 ;
+// num1 = 10;
+// num2 = 20;
+// let Sum = num1+num2;
+// console.log(Sum)
+
+// **9**
+// let num5 = 2;
+// let num6 = 3;
+// console.log(num5/num6);
+
+// **10**
+// let num3 = 2;
+// let num4 = 4;
+// let _sum = num3+num4;
+// let div = _sum/5;
+// console.log(div)
+
+// **11**
+// let f = 50;
+// let c = (f-32)*5/9;
+// console.log("The temperature in Celcius is ", c);
+
+// **12**
+// let a = prompt("Enter the first number: ");
+// let b = prompt("Enter the second number: ");
+// let c = (parseInt(a) + parseInt(b));
+// alert(c)
+
+// **13**
+// let d = prompt("Enter the first number: ");
+// let e = prompt("Enter the second number: ");
+// let Div = d/e;
+// console.log(Div) 
+
+// **14**
+// let f = prompt("Enter the first number: ");
+// let g = prompt("Enter the second number: ");
+// let Multiply = f*g;
+// alert(Multiply);
+
+// **15**
+// let num7 = prompt("Enter the first number:");
+// let num8 = prompt("Enter the second number:");
+// let quotient = parseFloat(num7) / parseFloat(num8);
+
+// console.log("The quotient of " + num7 + " and " + num8 + " is: " + quotient);
+
+// **16**
+// let num_9 = prompt("Enter the first number:");
+// let num_10 = prompt("Enter the second number:");
+// let reminder = num_9%num_10;
+// alert(reminder);
+
+
